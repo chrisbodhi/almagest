102050a215fd2b7f0430a585c7ef9a4e7d94b8b0
chrisbodhi <chris.boette@data.world>
2024-04-14 12:58:25 -0400

    Add symbols file, usage undetermined.

---
+++ src/symbols.zig
@@ -0,0 +1,60 @@
+// Eccentricity of an orbit
+pub var e = undefined;
+// Altitude above the horizon
+pub var h = undefined;
+// radius vector, or disstance of a body to the sun, in AU
+pub var r_AU = undefined;
+// True anomaly
+pub var v = undefined;
+
+// Azimuth
+pub var A = undefined;
+// Hour angle
+pub var H = undefined;
+// Mean anomaly
+pub var M = undefined;
+// Distance from Earth to sun, in AU
+pub var R_AU = undefined;
+// Time in Julian centuries (36525 days) from J2000.0
+pub var T = undefined;
+
+// Right ascension, measured in hours, minutes, and seconds
+pub var @"α" = undefined;
+// Declination
+pub var @"δ" = undefined;
+// Obliquity of the ecliptic
+pub var @"ε" = undefined;
+// Sidereal time
+pub var @"θ" = undefined;
+// Sidereal time at Greenwich
+pub var GST = undefined;
+// Parallax
+pub var @"π" = undefined;
+// Time in Julian millennia (36250 days) from J2000.0
+pub var @"τ" = undefined;
+// Geographical latitude
+pub var @"φ" = undefined;
+// Geocentric latitude
+pub var @"φ_prime" = undefined;
+
+// Distance to Earth, in AU
+pub var @"Δ" = undefined;
+// Difference in TD - UT (dynamical time and universal time)
+pub var @"ΔT" = undefined;
+// Nutation in obliquity
+pub var @"Δε" = undefined;
+// Nutation in longitude
+pub var @"Δψ" = undefined;
+
+// Astronomical unit
+pub var AU = undefined;
+// Integer part of a number
+pub var INT: i64 = undefined;
+// Julian Day
+pub var JD = undefined;
+// Julian Ephemeris Day
+pub var JDE = undefined;
+// Dynamical time
+pub var TD = undefined;
+// Universal time
+pub var UT = undefined;
